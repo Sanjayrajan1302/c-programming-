25994e61d11feda26d5e4e6dffecf8294e6a3900
Sanjayrajan1302 <128566467+Sanjayrajan1302@users.noreply.github.com>
2023-03-22 09:08:06 -0700

    Add files via upload

---
+++ program4.cpp
@@ -0,0 +1,10 @@
+//4. Predict the output or error(s) for the following program:
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int const a=5;
+	int const *p=&a;
+	printf("%d",+(*p));
+}
+
---
+++ program6.cpp
@@ -0,0 +1,12 @@
+//6. Predict the output or error(s) for the following program:
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char string[19]="Hello World";
+	display(string);
+}
+void display(string)
+{
+	printf("%s",string);
+}
